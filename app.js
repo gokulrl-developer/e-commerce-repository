6c522db35b52bd96e46b5b52a1e71a934e550ade
gokulrl-developer <gokulrlsreepath95@gmail.com>
2024-11-14 17:34:07 +0530

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express=require('express');
+const path=require('path');
+const {engine} = require('express-handlebars');
 const app=express();
+app.engine('hbs', engine({ 
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+ }));
+app.set('view engine', 'hbs');
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 app.listen(8000,()=>{
     console.log("...server starts running")
 })
